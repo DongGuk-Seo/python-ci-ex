683a51b31a6678218a8c0626f2d1bd6381099cf0
DongGuk-Seo <failnxid@gmail.com>
2022-08-29 10:55:57 +0900

    untoken

--- main_test.py
+++ main_test.py
@@ -1,2 +1,14 @@
 import unittest
 
+from main import hello
+
+class MainTest(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello World")
+
+if __name__ == "__main__":
+    unittest.main()
+    # python -m unittest discover -p "*_test.py"
+    # coverage run --source=./ -m unittest discover -p "*_test.py"
+    # coverage xml
+    
